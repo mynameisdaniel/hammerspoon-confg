5aab2a7b5179331d66a5497dd0d603a351b8f96e
Daniel Chan <daniel.chan@coupa.com>
2022-12-10 16:36:24 -0800

    init - add lua config

---
+++ init.lua
@@ -0,0 +1,139 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x 
+  f.y = max.y + (max.h / 2)
+  f.w = max.w 
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y 
+  f.w = max.w 
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "m", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y 
+  f.w = max.w 
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "9", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y 
+  f.w = max.w / 4
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "0", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 4)
+  f.y = max.y
+  f.w = max.w / 4
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 4
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "=", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w * .75)
+  f.y = max.y
+  f.w = max.w / 4
+  f.h = max.h
+  win:setFrame(f)
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "]", function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  win:moveToScreen(screen:next())
+end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "[", function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  win:moveToScreen(screen:previous())
+end)
+hs.hotkey.bind({"ctrl", "shift"}, "Up", function()
+  local screen = hs.mouse.getCurrentScreen()
+  local nextScreen = screen:next()
+  local rect = nextScreen:fullFrame()
+  local center = hs.geometry.rectMidPoint(rect)
+hs.mouse.setAbsolutePosition(center)
+end)
+hs.hotkey.bind({"ctrl", "shift"}, "Down", function()
+  local screen = hs.mouse.getCurrentScreen()
+  local nextScreen = screen:next()
+  local rect = nextScreen:fullFrame()
+  local center = hs.geometry.rectMidPoint(rect)
+hs.mouse.setAbsolutePosition(center)
+end)
+hs.hotkey.bind({"ctrl", "shift"}, "Left", function()
+  local cursorscreen = hs.mouse.getCurrentScreen()
+  local frame = cursorscreen:fullFrame()
+  local absoluteCenter = hs.geometry.rectMidPoint(frame)
+  local relativeCenter= cursorscreen:absoluteToLocal(absoluteCenter)
+  print(relativeCenter.x)
+  print(relativeCenter.y)
+hs.mouse.setRelativePosition(hs.geometry(0, relativeCenter.y))
+end)
+hs.hotkey.bind({"ctrl", "shift"}, "Right", function()
+  local cursorscreen = hs.mouse.getCurrentScreen()
+  local frame = cursorscreen:fullFrame()
+  local absoluteCenter = hs.geometry.rectMidPoint(frame)
+  local relativeCenter= cursorscreen:absoluteToLocal(absoluteCenter)
+hs.mouse.setRelativePosition(hs.geometry(relativeCenter.x*2, relativeCenter.y))
+end)
